f3532f331befc172768f44b10598bf9899962e3b
nht-cell <nht72773@gmail.com>
2025-12-12 00:34:05 -0600

    Update page.tsx

--- app/pre-made/page.tsx
+++ app/pre-made/page.tsx
@@ -45,6 +45,7 @@ export default function PreMadePage() {
               <div className="absolute inset-0 bg-gradient-to-r from-white/0 via-white/20 to-white/0 transform -skew-x-12 -translate-x-full group-hover:translate-x-full transition-transform duration-1000" />
             </Button>
           </a>
+        </div>
         
         <div className="mb-20">
           <ProductCarousel />
