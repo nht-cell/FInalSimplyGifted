4b08522a5b6854a8969c6992b56ca1d3bfe157d8
nht-cell <nht72773@gmail.com>
2025-12-12 00:19:25 -0600

    Update page.tsx

--- app/pre-made/page.tsx
+++ app/pre-made/page.tsx
@@ -42,7 +42,7 @@ export default function PreMadePage() {
           >
             import Link from 'next/link';
 
-<Link href="/https://buy.stripe.com/7sY8wP8gWh1h4b07dv7Vm00">
+<Link href="/https://buy.stripe.com/7sY8wP8gWh1h4b07dv7Vm08">
   <Button
     size="lg"
     className="relative bg-gradient-to-r from-accent to-accent/80 hover:from-accent/90 hover:to-accent/70 text-white px-16 py-7 text-xl"
