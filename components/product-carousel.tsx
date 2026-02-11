998b8f5024c7f7d38209a1808d7e8a1be79a3836
nht-cell <nht72773@gmail.com>
2026-02-10 21:06:51 -0600

    Update product image in product carousel

--- components/product-carousel.tsx
+++ components/product-carousel.tsx
@@ -9,7 +9,7 @@ const products = [
     id: 1,
     name: "Self-Care Basket",
     price: "$14.99",
-    image: "/self-care-min.png",
+    image: "/Cupid26.png",
     description:
       "This Valentine’s, make someone’s day unforgettable with a personalized Cupid’s Basket! Perfect for your girlfriend, best friend, or secret crush, these baskets are crafted with love and designed just for you.",
   },
